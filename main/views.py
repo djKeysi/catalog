6cbab63de58d058749b116a99513cec71cbeda43
Зенченко Евгений <zenchenko_es@outlook.com>
2023-08-28 18:25:12 +0700

    end homework2

--- main/views.py
+++ main/views.py
@@ -8,5 +8,5 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'You have new message from {name1}({phone}): {message}')
+        print(f'You have new message from {name}({phone}): {message}')
     return render(request,'main/contacts.html')
