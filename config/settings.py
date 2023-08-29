b2f61ebddd9cda27351d9b571c8b947a0fc57e86
Зенченко Евгений <zenchenko_es@outlook.com>
2023-08-29 20:26:46 +0700

    end homework2

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'USER': 'postgres',
+        'PASSWORD':128943
+        #'HOST':
+        #'PORT':5432
+
     }
 }
 
@@ -129,3 +134,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
