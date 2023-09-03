3444e5770f0f96fcdf60409a92db5a4ba59400a3
Зенченко Евгений <zenchenko_es@outlook.com>
2023-09-03 09:17:32 +0700

    homework2 add

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catalog',
+        'USER': 'postgres',
+        'PASSWORD':128943
+        #'HOST':
+        #'PORT':5432
+
     }
 }
 
--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category:
+    name = models.CharField(max_length=100)
+    discription = models.TextField(max_length=200)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    discription = models.TextField(max_length=200)
+    picture = models.ImageField(upload_to='picture/')
+    category = models.ForeignKey(Category)
+    price_for_buy = models.IntegerField()
+    data_create= models.DateTimeField()
+    last_modified_date = models.DateField()# c установкой при изменении
+
+
+
+
+
