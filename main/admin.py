2646cee05d08e4af54db4e782771cd8b13f6aed2
Зенченко Евгений <zenchenko_es@outlook.com>
2023-09-03 12:50:19 +0700

    homework2 add

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
+from main.models import Product, Category
+
+
 # Register your models here.
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('first_name','last_name','is_active',)
-    list_filter = ('is_active',)
-    search_fields = ('first_name','last_name',)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'category', 'price_for_buy', 'data_create', 'last_modified_date')
+    list_filter = ('name', 'category', 'discription')
+    # search_fields = ('first_name','last_name',)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'discription')
+    #list_filter = ('name', 'category', 'description')
+    # search_fields = ('first_name','last_name',)
+
--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
-    discription = models.TextField(max_length=200, verbose_name='Описание')
+    discription = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return f'{self.name} {self.discription}'
@@ -17,13 +17,13 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     discription = models.TextField(max_length=200, verbose_name='Описание')
     picture = models.ImageField(upload_to='picture/', null=True, blank=True, verbose_name='Изображение')
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price_for_buy = models.IntegerField(verbose_name='цена за покупку')
     data_create = models.DateTimeField(auto_now_add=True, blank=True, verbose_name='дата создания')
     last_modified_date = models.DateTimeField(auto_now=True, blank=True, verbose_name='дата последнего изменения')  # c установкой при изменении
-
+    #created_at = models.CharField(max_length=100, verbose_name='Создание', null=True, blank=True)
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.name}{self.discription}'
 
     class Meta:
         verbose_name = 'продукт'
