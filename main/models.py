c37f8305e9305f837e149af4a18909e31031ca07
Зенченко Евгений <zenchenko_es@outlook.com>
2023-09-03 11:15:30 +0700

    homework2 add

--- main/models.py
+++ main/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 
 
-class Category:
-    name = models.CharField(max_length=100)
-    discription = models.TextField(max_length=200)
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    discription = models.TextField(max_length=200, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} {self.discription}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    discription = models.TextField(max_length=200)
-    picture = models.ImageField(upload_to='picture/')
-    category = models.ForeignKey(Category)
-    price_for_buy = models.IntegerField()
-    data_create= models.DateTimeField()
-    last_modified_date = models.DateField()# c установкой при изменении
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    discription = models.TextField(max_length=200, verbose_name='Описание')
+    picture = models.ImageField(upload_to='picture/', null=True, blank=True, verbose_name='Изображение')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price_for_buy = models.IntegerField(verbose_name='цена за покупку')
+    data_create = models.DateTimeField(auto_now_add=True, blank=True, verbose_name='дата создания')
+    last_modified_date = models.DateTimeField(auto_now=True, blank=True, verbose_name='дата последнего изменения')  # c установкой при изменении
 
+    def __str__(self):
+        return f'{self.name}'
 
-
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
 
 
