e77dbb8534824e94b541b2852e369eb30d78a9d3
Зенченко Евгений <zenchenko_es@outlook.com>
2023-09-09 22:31:12 +0700

    materials

--- main/urls.py
+++ main/urls.py
@@ -2,12 +2,15 @@ import contact as contact
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import  contact, StudentListView
+from main.views import  contact, StudentListView,StudentCreateView,StudentDetailView,StudentUpdateView
 
 app_name = MainConfig.name
 
 
 urlpatterns = [
     path('', StudentListView.as_view(), name = 'index'),
-    path('contact/', contact, name = 'contact')
+    path('contact/', contact, name = 'contact'),
+    path('view/<int:pk>/',StudentDetailView.as_view(),name='view_student'),
+    path('create/', StudentCreateView.as_view(), name = 'create_student'),
+    path('edit/<int:pk>/', StudentUpdateView.as_view(), name = 'update_student')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 from main.models import Student
 
@@ -16,8 +17,25 @@ from main.models import Student
 #     return render(request,'main/contacts.html')
 class StudentListView(ListView):
     model = Student
-    template_name = 'main/home.html'
+    #template_name = 'main/index.html'  # student_detail
 
+
+class StudentDetailView(DetailView):
+    model = Student
+    #template_name = 'main/student_detail.html'
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    # template_name = 'main/student_form.html'
+    fields = ('first_name', 'last_name', 'avatar')
+    success_url = reverse_lazy('main:index')
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    # template_name = 'main/student_form.html'
+    fields = ('first_name', 'last_name', 'avatar')
+    success_url = reverse_lazy('main:index')
 
 
 # def home(request):
@@ -37,6 +55,4 @@ def contact(request):
     context = {
         'title': 'Контакты'
     }
-    return render(request,'main/contact.html',context)
-
-
+    return render(request, 'main/contact.html', context)
